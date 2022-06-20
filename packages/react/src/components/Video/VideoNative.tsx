0c27acee4bf68c06ec2b08fe5f749dec62ff4d6a
niravcodes <mail@nirav.com.np>
2022-06-21 00:36:26 +0545

    Add React Native Component for review

---
+++ packages/react/src/components/Video/VideoNative.tsx
@@ -0,0 +1,52 @@
+import "react-native-get-random-values";
+import { WebRTC } from "react-native-webrtc";
+import VideoBase from "./VideoBase";
+import { IVideoCommonProps } from "./IVideoCommonProps";
+import { Video } from "@signalwire/js";
+import { useState } from "react";
+interface IVideoNativeProps extends IVideoCommonProps {
+  rtcViewStyle: React.CSSProperties;
+}
+
+const VideoNative: React.FC<IVideoNativeProps> = ({
+  token,
+  onRoomUpdate = () => {},
+  onEvent = (eventName, event) => {
+    console.log(eventName, event);
+  },
+  rtcViewStyle = {
+    top: 0,
+    position: "absolute",
+    height: "100%",
+    width: "100%",
+  },
+  ...props
+}) => {
+  const [roomSession, setRoomSession] = useState<Video.RoomSession | null>(
+    null
+  );
+  return (
+    <VideoBase
+      token={token}
+      onRoomJoined={(roomSession, memberId) => {
+        console.log("Room was joined");
+        setRoomSession(roomSession);
+      }}
+      onEvent={(eventName, event) => {
+        // eventName handler here if any
+        onEvent(eventName, event);
+      }}
+    >
+      <div {...props}>
+        {roomSession !== null && roomSession.remoteStream && (
+          <RTCView
+            streamURL={roomSession.remoteStream.toURL()}
+            style={{ ...rtcViewStyle }}
+          />
+        )}
+      </div>
+    </VideoBase>
+  );
+};
+
+export default VideoNative;
