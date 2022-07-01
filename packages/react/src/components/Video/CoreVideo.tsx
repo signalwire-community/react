25cc10a164b4308e78fae6c3bd5beb009c84687f
Daniele Di Sarli <danieleds0@gmail.com>
2022-07-01 15:10:35 +0200

    fix imports

--- packages/react/src/components/Video/CoreVideo.tsx
+++ packages/react/src/components/Video/CoreVideo.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { RefObject, useEffect, useState } from 'react';
 import * as SignalWire from '@signalwire/js';
 import { IVideoProps } from './IVideoProps';
 
