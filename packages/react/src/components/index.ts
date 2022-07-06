98c221014c2033fd53444f30011fd4bd1d3b66a6
Daniel Di Sarli <danieleds0@gmail.com>
2022-07-06 17:41:53 +0200

    export CoreVideo (#7)

--- packages/react/src/components/index.ts
+++ packages/react/src/components/index.ts
@@ -1,3 +1,12 @@
+import CoreVideo from './Video/CoreVideo'
+
 export { default as RoomPreview } from './RoomPreview';
 export { default as Video } from './Video';
 export { default as VideoConference } from './VideoConference';
+
+/** @ignore */
+export const __internal = {
+    Video: {
+        CoreVideo
+    }
+}
