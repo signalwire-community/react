e13fc3e16ad1fdf80172aac04127f8fec094e1e9
Daniele Di Sarli <danieleds0@gmail.com>
2022-07-06 17:39:35 +0200

    ignore

--- packages/react/src/components/index.ts
+++ packages/react/src/components/index.ts
@@ -4,6 +4,7 @@ export { default as RoomPreview } from './RoomPreview';
 export { default as Video } from './Video';
 export { default as VideoConference } from './VideoConference';
 
+/** @ignore */
 export const __internal = {
     Video: {
         CoreVideo
