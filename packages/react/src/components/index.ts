bceb2a553c109f1f686fc769721f26d9358805c4
Daniele Di Sarli <danieleds0@gmail.com>
2022-07-08 12:13:18 +0200

    export ICoreVideoProps

--- packages/react/src/components/index.ts
+++ packages/react/src/components/index.ts
@@ -1,12 +1,14 @@
-import CoreVideo from './Video/CoreVideo'
+import _CoreVideo from './Video/CoreVideo';
+import { ICoreVideoProps as _ICoreVideoProps } from './Video/CoreVideo';
 
 export { default as RoomPreview } from './RoomPreview';
 export { default as Video } from './Video';
 export { default as VideoConference } from './VideoConference';
 
 /** @ignore */
-export const __internal = {
-    Video: {
-        CoreVideo
+export namespace __internal {
+    export namespace Video {
+        export const CoreVideo = _CoreVideo
+        export type ICoreVideoProps = _ICoreVideoProps
     }
 }
