c72ffb18630c3608effc589edf983f8d67ae0a28
Daniele Di Sarli <danieleds0@gmail.com>
2022-07-21 11:41:53 +0200

    new videonative demo

--- demo/videonative/App.js
+++ demo/videonative/App.js
@@ -1,19 +1,57 @@
+const TOKEN = `eyJ0eXAiOiJWUlQiLCJjaCI6InJlbGF5LnNpZ25hbHdpcmUuY29tIiwiYWxnIjoiSFM1MTIifQ.eyJpYXQiOjE2NTgzODkxNzEsImp0aSI6ImE4NjE3NDkzLTVkZDEtNGQ1ZS1iNjEzLWYyODllZDQ0MDljMCIsInN1YiI6ImExNmQ4ZjllLTIxNjYtNGU4Mi05NmFmLWE0ODQwZjIxN2JjMyIsInUiOiJkYW5pZWxlIiwiamEiOiJtZW1iZXIiLCJyIjoiZW1iZWRkYWJsZSIsInMiOlsicm9vbS5saXN0X2F2YWlsYWJsZV9sYXlvdXRzIiwicm9vbS5zZWxmLmF1ZGlvX211dGUiLCJyb29tLnNlbGYuYXVkaW9fdW5tdXRlIiwicm9vbS5zZWxmLnZpZGVvX211dGUiLCJyb29tLnNlbGYudmlkZW9fdW5tdXRlIiwicm9vbS5zZWxmLmRlYWYiLCJyb29tLnNlbGYudW5kZWFmIiwicm9vbS5zZWxmLnNldF9pbnB1dF92b2x1bWUiLCJyb29tLnNlbGYuc2V0X291dHB1dF92b2x1bWUiLCJyb29tLnNlbGYuc2V0X2lucHV0X3NlbnNpdGl2aXR5Iiwicm9vbS5oaWRlX3ZpZGVvX211dGVkIiwicm9vbS5zaG93X3ZpZGVvX211dGVkIl0sImFjciI6dHJ1ZSwibWEiOiJhbGwiLCJlcnAiOnRydWUsIm10YSI6e30sInJtdGEiOnt9fQ.CDDT2GGZ0IybCiDyRaKf0NwBp-JwZtrXz1X6itKtsCnfbYSPrM2WOpOBjhTeIl7IXyuFAPk4iTCWm76q58g8vQ`;
 import 'react-native-get-random-values';
+import React, {useEffect, useState} from 'react';
 import {SafeAreaView, View} from 'react-native';
-import React, {useEffect, useRef, useState} from 'react';
-import {VideoNative} from '@signalwire-community/react-native';
-import {RTCView} from 'react-native-webrtc';
+
+import {
+	ScreenCapturePickerView,
+	RTCPeerConnection,
+	RTCIceCandidate,
+	RTCSessionDescription,
+	RTCView,
+	MediaStream,
+	MediaStreamTrack,
+	mediaDevices,
+	registerGlobals
+} from 'react-native-webrtc';
+
+import {Video} from '@signalwire/js';
+
+// registerGlobals();  // Apparently this is not needed?
 
 const App = () => {
+  const [roomSession, setRoomSession] = useState(null);
+  const [setup, setSetup] = useState(false);
+  const [localStream, setLocalStream] = useState(null);
+  const [remoteStream, setRemoteStream] = useState(null);
+  useEffect(() => {
+    async function dothing() {
+      if (setup) return console.log('Setup done');
+      setSetup(true);
+      const room = new Video.RoomSession({
+        token: TOKEN,
+        audio: true,
+        video: true,
+      });
+      room.on('room.joined', e => {
+        setRoomSession(room);
+        setLocalStream(room.localStream);
+        setRemoteStream(room.remoteStream);
+      });
+      console.log('Waiting to join room');
+      await room.join();
+      console.log('room.join() completed');
+    }
+    dothing();
+  }, [roomSession]);
+
   return (
-    <SafeAreaView>
-      <VideoNative
-        token={
-          'eyJ0eXAiOiJWUlQiLCJjaCI6InJlbGF5LnNpZ25hbHdpcmUuY29tIiwiYWxnIjoiSFM1MTIifQ.eyJpYXQiOjE2NTc3MjQzMjksImp0aSI6IjFjYWE2Y2Q0LWNiMTEtNDI5OS04MzI1LWI5YThmMTVjMDMwZiIsInN1YiI6ImMzYWQ3NzE2LTAxYjQtNGMzZi05N2FiLWRmMjIxMTM1YWFiMiIsInUiOiJuaXIiLCJqYSI6Im1lbWJlciIsInIiOiJzYXVyYXYiLCJzIjpbInJvb20ubGlzdF9hdmFpbGFibGVfbGF5b3V0cyIsInJvb20uc2VsZi5hdWRpb19tdXRlIiwicm9vbS5zZWxmLmF1ZGlvX3VubXV0ZSIsInJvb20uc2VsZi52aWRlb19tdXRlIiwicm9vbS5zZWxmLnZpZGVvX3VubXV0ZSIsInJvb20uc2VsZi5kZWFmIiwicm9vbS5zZWxmLnVuZGVhZiIsInJvb20uc2VsZi5zZXRfaW5wdXRfdm9sdW1lIiwicm9vbS5zZWxmLnNldF9vdXRwdXRfdm9sdW1lIiwicm9vbS5zZWxmLnNldF9pbnB1dF9zZW5zaXRpdml0eSIsInJvb20uaGlkZV92aWRlb19tdXRlZCIsInJvb20uc2hvd192aWRlb19tdXRlZCJdLCJhY3IiOnRydWUsIm1hIjoiYWxsIiwiZXJwIjp0cnVlLCJtdGEiOnt9LCJybXRhIjp7fX0.JRaFA05vI4cn5nLqdUBvocFRMdysGWEXcb-FD-pQsGcfcMudmYGXle8S5GotFUGd8vKhOEmYKNz5GH0nBVBKqQ'
-        }
-      />
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1 }}>
+        {remoteStream && <RTCView streamURL={remoteStream.toURL()} style={{ flex: 1 }} />}
+        {localStream && <RTCView streamURL={localStream.toURL()} style={{ flex: 1 }} />}
+      </View>
     </SafeAreaView>
   );
 };
-
 export default App;
