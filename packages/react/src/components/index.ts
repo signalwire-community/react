d49eb0febd69875b543b53a6446a7fb5b2904182
Daniele Di Sarli <danieleds0@gmail.com>
2022-08-05 18:39:13 +0200

    components for react

--- packages/react/src/components/index.ts
+++ packages/react/src/components/index.ts
@@ -1,6 +1,8 @@
 import _CoreVideo from './Video/CoreVideo';
 import { ICoreVideoProps as _ICoreVideoProps } from './Video/CoreVideo';
 
+export { default as LocalStream } from './LocalStream';
+export { default as RemoteStream } from './RemoteStream';
 export { default as RoomPreview } from './RoomPreview';
 export { default as Video } from './Video';
 export { default as VideoConference } from './VideoConference';
