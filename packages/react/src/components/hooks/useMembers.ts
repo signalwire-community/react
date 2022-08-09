2e34bd05d0988cd074bea9f9b6ab13d1a3c412d6
niravcodes <mail@nirav.com.np>
2022-08-09 22:52:09 +0545

    Add methods to member objects

--- packages/react/src/components/hooks/useMembers.ts
+++ packages/react/src/components/hooks/useMembers.ts
@@ -7,19 +7,62 @@ export default function useMembers(roomSession: Video.RoomSession | null) {
   const [members, setMembers] = useState<any>([]);
 
   useEffect(() => {
+    if (roomSession === null || roomSession === undefined) return;
+    function addMethods(members: any[]) {
+      // TODO: Add error handling to these methods
+      return members.map((m) => {
+        return {
+          ...m,
+          audio: {
+            muted: m.audio_muted,
+            mute: () => roomSession?.audioMute({ memberId: m.id }),
+            unmute: () => roomSession?.audioUnmute({ memberId: m.id }),
+            toggle: () => {
+              m.audio_muted
+                ? roomSession?.audioUnmute({ memberId: m.id })
+                : roomSession?.audioMute({ memberId: m.id });
+            },
+          },
+          video: {
+            muted: m.video_muted,
+            mute: () => roomSession?.videoMute({ memberId: m.id }),
+            unmute: () => roomSession?.videoUnmute({ memberId: m.id }),
+            toggle: () => {
+              m.video_muted
+                ? roomSession?.videoUnmute({ memberId: m.id })
+                : roomSession?.videoMute({ memberId: m.id });
+            },
+          },
+          speaker: {
+            muted: m.deaf,
+            toggle: () =>
+              m.deaf
+                ? roomSession?.undeaf({ memberId: m.id })
+                : roomSession?.deaf({ memberId: m.id }),
+            mute: () => roomSession?.deaf({ memberId: m.id }),
+            unmute: () => roomSession?.undeaf({ memberId: m.id }),
+          },
+          remove: () => {
+            roomSession?.removeMember({ memberId: m.id });
+          },
+        };
+      });
+    }
     if (roomSession === null) return;
-    roomSession.on("room.joined", (e) => {
-      selfId.current = (e as any).member_id;
+    roomSession.on("room.joined", (e: any) => {
+      selfId.current = e.member_id;
+      console.log(selfId.current);
+      let members = e.room_session.members;
+      setMembers(addMethods(members));
     });
     roomSession.on("memberList.updated", (e) => {
-      setMembers(e.members);
+      setMembers(addMethods(e.members));
     });
     return () => {
       roomSession.removeAllListeners();
     };
   }, [roomSession]);
 
-  // TODO: supplement with extra methods inside each member to remove and mute them and such
   return {
     self: members.find((m: any) => m.id === selfId.current) ?? null,
     members,
