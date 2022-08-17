9547060622da1aee165702eeaf4e60be45f85d60
Daniele Di Sarli <danieleds0@gmail.com>
2022-08-17 12:49:28 +0200

    image room preview

--- packages/react-native/src/components/RoomPreview/index.tsx
+++ packages/react-native/src/components/RoomPreview/index.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { View } from "react-native";
-import { WebView } from 'react-native-webview';
+import { Image, ImageErrorEventData, ImageProps, ImageStyle, NativeSyntheticEvent, StyleProp } from "react-native";
 
 type RoomPreviewProps = {
   /** Image to show while the room preview is loading. */
@@ -8,15 +7,16 @@ type RoomPreviewProps = {
   /** URL of the room preview. */
   previewUrl?: { uri: string };
   /** Custom styles. */
-  style?: any; // ConstructorParameters<typeof WebView>[0]['style'];
+  style?: StyleProp<ImageStyle> | undefined;
 };
+
+const BLACK_IMG = { uri: "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=" }
 
 const RoomPreview: React.FC<RoomPreviewProps> = ({
   previewUrl,
   loadingUrl,
   style,
 }) => {
-  const webView = useRef<WebView>(null)
   const [currUrl, setCurrUrl] = useState<{ uri: string } | undefined>(loadingUrl);
 
   useEffect(() => {
@@ -31,21 +31,9 @@ const RoomPreview: React.FC<RoomPreviewProps> = ({
     let intv: NodeJS.Timer | null = null;
 
     if (previewUrl) {
-      console.log("Setting Interval", previewUrl)
-      intv = setInterval(() => {
-        setCurrUrl(currUrl => {
-          if (currUrl?.uri === previewUrl?.uri) {
-            // We are already displaying previewUrl, so just refresh it.
-            // webView.current?.clearCache(true)
-            // webView.current?.reload()
-
-            return {...currUrl}
-          } else {
-            // We probably were displaying the "loading" image, so now we should
-            // try displaying the previewUrl.
-            return previewUrl
-          }
-        })
+      intv = setInterval(async () => {
+        // Update the object reference to force a refresh
+        setCurrUrl({ ...previewUrl })
       }, 10000);
     }
 
@@ -56,26 +44,91 @@ const RoomPreview: React.FC<RoomPreviewProps> = ({
     };
   }, [previewUrl]);
 
-  function onHttpError() {
+  function onError() {
     if (currUrl?.uri !== loadingUrl?.uri) {
       setCurrUrl(loadingUrl)
     }
   }
 
   return (
-    <View style={{
-      aspectRatio: 16 / 9,
-      backgroundColor: "blue",
-      ...style,
-    }}>
-      <WebView
-        ref={webView}
-        source={currUrl}
-        // cacheEnabled={false}
-        onHttpError={onHttpError}
-      />
-    </View>
+    <EtagImage
+      source={currUrl ?? BLACK_IMG}
+      onError={onError}
+      fadeDuration={0}
+      style={[
+        {
+          aspectRatio: 16 / 9,
+          backgroundColor: "black",
+        },
+        style
+      ]}
+    />
   );
 };
+
+/**
+ * This component is similar to the Image component, but has some additional caching checks.
+ * 
+ * In particular, whenever you set the `source` property, this component:
+ * 
+ *  1. Makes a HEAD request to check if the Etag of the resouce changed.
+ *  2. If the Etag changed, downloads the new image (while still displaying the old one)
+ *  3. Replaces the old image with the new image
+ * 
+ * This ensures that
+ * 
+ *  - When changing image, no "empty image" is displayed while downloading the new one.
+ */
+function EtagImage(props: Omit<ImageProps, 'source' | 'onError'> & { source: { uri?: string }, onError?: ((error: NativeSyntheticEvent<ImageErrorEventData> | null) => void) | undefined }) {
+  const [source, setSource] = useState(props.source)
+  const lastEtag = useRef<string | null>(null)
+
+  useEffect(() => {
+    if (!props.source || !props.source.uri || props.source.uri.startsWith('data')) {
+      setSource(props.source)
+    } else {
+      (async () => {
+        try {
+          const response = await fetch(props.source.uri!, { method: "HEAD" })
+          const etag = response.headers.get('etag')
+          if (etag !== lastEtag.current) {
+            // The image changed, we need to refresh it.
+            setSource({
+              uri: (await urlContentToDataUriNoCache(props.source.uri!)).toString()
+            })
+            lastEtag.current = etag
+          }
+        } catch (e) {
+          props.onError?.(null)
+        }
+      })()
+    }
+  }, [props.source])
+
+  return <Image
+    {...props}
+    source={source}>
+  </Image>
+}
+
+/**
+ * Takes a URL and downloads it as a data URI. This function does NOT use cache
+ * for the network request. 
+ */
+function urlContentToDataUriNoCache(url: string): Promise<string | ArrayBuffer> {
+  return fetch(url, {
+    headers: new Headers({
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      'Pragma': 'no-cache',
+      'Expires': '0'
+    })
+  })
+    .then(response => response.blob())
+    .then(blob => new Promise(callback => {
+      let reader = new FileReader();
+      reader.onload = function () { callback(this.result) };
+      reader.readAsDataURL(blob);
+    }));
+}
 
 export default RoomPreview;
