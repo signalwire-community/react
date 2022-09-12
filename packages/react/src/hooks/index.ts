aae0fb7b5d4e168bfa5dcb66569e1963a211f12f
Daniel Di Sarli <danieleds0@gmail.com>
2022-09-12 17:43:30 +0200

    refactor: move hooks folder into top-level dir (#22)

---
+++ packages/react/src/hooks/index.ts
@@ -0,0 +1,5 @@
+export { default as useLayouts } from "./useLayouts";
+export { default as useMembers } from "./useMembers";
+export { usePermissions } from "./usePermissions";
+export { default as useScreenShare } from "./useScreenShare";
+export { default as useWebRTC } from "./useWebRTC";
