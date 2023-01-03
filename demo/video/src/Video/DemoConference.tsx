8f0ef58b7b9ec10e99d60afedbbb60a2cda4fff3
Nirav Nikhil <niravnikhil@gmail.com>
2023-01-03 16:20:14 +0545

    fix: occupy maximum space of the containing div for PVC (#41)
    * make pvc occupy the full space of its container div
    
    * use the new way to maximize pvc

--- demo/video/src/Video/DemoConference.tsx
+++ demo/video/src/Video/DemoConference.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
-import { VideoConference } from '@signalwire-community/react';
+import { useState } from "react";
+import { VideoConference } from "@signalwire-community/react";
 
 function DemoConference() {
   return (
-    <div style={{ maxHeight: '100vh', aspectRatio: '16/9' }}>
+    <div style={{ height: "100vh", width: "100vw" }}>
       <VideoConference token="vpt_78f91a752d4d9c685e47bd4a19fe72c1" />
     </div>
   );
