2e891a65ba1b4f3486ba6e8b084f88ab1e4ed9d3
niravcodes <mail@nirav.com.np>
2023-02-02 16:29:54 +0545

    feat: add Fabric namespace support

--- demo/video/src/Fabric/DemoFabric.tsx
+++ demo/video/src/Fabric/DemoFabric.tsx
@@ -2,17 +2,14 @@ import SignalWire from "@signalwire/js";
 import { Fabric } from "@signalwire-community/react";
 import { useState } from "react";
 
-let token =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJkMTVhOWE3ZS1kNTIwLTQ3NzAtYjM5ZS00ZWQ1M2ZiNTQxZjQiLCJleHAiOjE2Nzc4NTg2ODgsInN1YnNjcmliZXJfaWQiOiIwMzJhYjNiYS1kY2JjLTRlNjQtYjA0Ny1lZjhkODZkZTUxOWIiLCJwcm9qZWN0X2lkIjoiNTA2Y2VhMzMtZWI0Ny00YjU2LTgyYjAtZDNjNWFmYWYzOWQ0IiwiY2xpZW50X2lkIjoiUHV6Q2NIbFZQSXNhZTNIcXVGdk50ZUZjSkNWeC01TkZEa2RtQlZORDZoSSJ9.JqsEC2XLF9VEfPuNmgkAEwGEJVQF_R6mVWHJas-ckB7Xxjaw0QnkpPbJxH0XVHlA5YruQ23rAuVIbv4KyYr9cQ";
-
 function DemoFabric() {
   const [roomSession, setRoomSession] =
     useState<SignalWire.Video.RoomSession | null>(null);
 
   return (
     <div>
-      {import.meta.env.VITE_ROOM_TOKEN ? (
-        <Fabric.Video token={token} />
+      {import.meta.env.VITE_FABRIC_TOKEN ? (
+        <Fabric.Video token={import.meta.env.VITE_FABRIC_TOKEN} />
       ) : (
         "No Token Present. Please set your env variable"
       )}
