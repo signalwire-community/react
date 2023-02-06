a012a8c246b2751c4a69d20cee2482e82682e814
Daniele Di Sarli <danieleds0@gmail.com>
2023-02-06 22:53:19 +0100

    tweak export

--- packages/react-native/src/components/index.ts
+++ packages/react-native/src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as LocalStream } from './LocalStream';
 export { default as RemoteStream } from './RemoteStream';
 export { default as Video } from './Video';
-export * as Fabric from './Fabric';
+import * as Fabric from './Fabric';
+export { Fabric };
