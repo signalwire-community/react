2a5fd4121cec603da981710c9b00990e4befe71b
niravcodes <mail@nirav.com.np>
2023-02-06 12:59:53 +0545

    create fabric namespace for react native

--- packages/react-native/src/components/Fabric/Video.tsx
+++ packages/react-native/src/components/Fabric/Video.tsx
@@ -1,36 +1,31 @@
 import 'react-native-get-random-values';
 import React, { useState } from 'react';
 import type * as SignalWire from '@signalwire/js';
-import {
-  __internal as Internal,
-  IVideoProps,
-} from '@signalwire-community/react';
+import { __internal as Internal, Fabric } from '@signalwire-community/react';
 
 import { registerGlobals } from 'react-native-webrtc';
 import RemoteStream from '../RemoteStream';
 registerGlobals();
 
-interface IVideoNativeProps extends IVideoProps {
+interface IVideoNativeProps extends Fabric.IVideoProps {
   style: any;
 }
 
-export default function Video({
-  style,
-  onRoomReady,
-  ...props
-}: IVideoNativeProps) {
+export function Video({ style, onRoomReady, ...props }: IVideoNativeProps) {
   const [roomSession, setRoomSession] =
     useState<SignalWire.Video.RoomSession | null>(null);
 
   return (
-    <Internal.Video.CoreVideo
+    <Fabric.CoreVideo
       {...props}
       onRoomReady={(r: SignalWire.Video.RoomSession) => {
         setRoomSession(r);
         onRoomReady?.(r);
       }}
+      audio={props.audio ?? true}
+      video={props.video ?? true}
     >
       <RemoteStream roomSession={roomSession ?? undefined} style={style} />
-    </Internal.Video.CoreVideo>
+    </Fabric.CoreVideo>
   );
 }
