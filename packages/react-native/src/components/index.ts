13cf02e58f4f7abfc3590374737fb8a57a752aa3
niravcodes <mail@nirav.com.np>
2023-02-07 00:11:26 +0545

    export fabric from react-native

--- packages/react-native/src/components/index.ts
+++ packages/react-native/src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as LocalStream } from './LocalStream';
 export { default as RemoteStream } from './RemoteStream';
 export { default as Video } from './Video';
+export * as Fabric from './Fabric';
