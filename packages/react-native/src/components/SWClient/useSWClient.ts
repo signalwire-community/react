c84d7178674911eaabdd4ac7fd5a8daa920d2d4b
niravcodes <niravnikhil@gmail.com>
2023-05-17 15:22:56 +0545

    use id returned by registerdevice for unregisterdevice

--- packages/react-native/src/components/SWClient/useSWClient.ts
+++ packages/react-native/src/components/SWClient/useSWClient.ts
@@ -7,12 +7,14 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 // helper to make a ts property optional, use same as Omit<>
 type PartiallyOptional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
 
-interface IRegisterParams
-  extends PartiallyOptional<
-    Parameters<SWClientOriginal['registerDevice']>[0],
-    'deviceType'
-  > {}
-type IUnregisterParams = Parameters<SWClientOriginal['unregisterDevice']>;
+type IRegisterParams = PartiallyOptional<
+  Parameters<SWClientOriginal['registerDevice']>[0],
+  'deviceType'
+>;
+type IUnregisterParams = PartiallyOptional<
+  Parameters<SWClientOriginal['unregisterDevice']>[0],
+  'id'
+>;
 
 type clientParams = Parameters<typeof ReactWrapper.useSWClient>;
 
@@ -20,7 +22,7 @@ interface ISWClientRN extends SWClientOriginal {
   _registerDevice: SWClientOriginal['registerDevice'];
   _unregisterDevice: SWClientOriginal['unregisterDevice'];
   registerDevice: (_: IRegisterParams) => Promise<any>;
-  unregisterDevice: (..._: IUnregisterParams) => Promise<any>;
+  unregisterDevice: (_: IUnregisterParams) => Promise<any>;
 }
 
 export default function useSWClient(...params: clientParams) {
@@ -51,7 +53,7 @@ export default function useSWClient(...params: clientParams) {
         console.error(e);
       }
 
-      if (result && result['push_notification_key'] !== undefined) {
+      if (result && result['push_notification_key'] && result['id']) {
         try {
           await AsyncStorage.setItem(
             '@push_notification_key',
@@ -60,18 +62,39 @@ export default function useSWClient(...params: clientParams) {
         } catch (e) {
           console.error('Could not save push notification decryption key.');
         }
+
+        try {
+          await AsyncStorage.setItem('@registration_id', result['id']);
+        } catch (e) {
+          console.error('Could not save the registration ID');
+        }
       }
 
       return result;
     };
 
-    client.unregisterDevice = async (
-      ...unregisterParams: IUnregisterParams
-    ) => {
+    client.unregisterDevice = async (unregisterParams: IUnregisterParams) => {
       let result;
 
+      // Look at AsyncStorage only if id is not provided as a param
+      if (unregisterParams.id === undefined) {
+        let id = null;
+        try {
+          id = await AsyncStorage.getItem('@registration_id');
+        } catch (e) {
+          console.error('Could not get the registration ID');
+        }
+
+        if (id !== null) unregisterParams.id = id;
+        else {
+          console.error('No ID provided to unregister device.');
+          return;
+        }
+      }
+
       try {
-        result = await client._unregisterDevice(...unregisterParams);
+        // @ts-expect-error id might be undefined.
+        result = await client._unregisterDevice(unregisterParams);
       } catch (e) {
         console.error('Could not unregister device');
         return result;
@@ -79,10 +102,9 @@ export default function useSWClient(...params: clientParams) {
 
       try {
         await AsyncStorage.removeItem('@push_notification_key');
+        await AsyncStorage.removeItem('@registration_id');
       } catch (e) {
-        console.error(
-          'Could not remove saved push notification decryption key.'
-        );
+        console.error('Could not remove saved items from async storage.');
       }
 
       return result;
