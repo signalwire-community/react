ec64cd78c4ebb41f08c36300b8b1a80a9c60e7d4
Nirav <niravnikhil@gmail.com>
2023-05-31 19:35:35 +0545

    export everything as default as per signalwire/cloud-product#6955

--- packages/react-native/src/components/index.ts
+++ packages/react-native/src/components/index.ts
@@ -1,8 +1,17 @@
-export { default as LocalStream } from './LocalStream';
-export { default as RemoteStream } from './RemoteStream';
-export { default as Video } from './Video';
-export { useSWClient } from './SWClient';
-export { useAddresses } from '@signalwire-community/react';
-
+import LocalStream from './LocalStream';
+import RemoteStream from './RemoteStream';
+import Video from './Video';
+import { useSWClient } from './SWClient';
+import { useAddresses } from '@signalwire-community/react';
 import * as Fabric from './Fabric';
-export { Fabric };
+
+export default {
+  LocalStream,
+  RemoteStream,
+  Video,
+  useSWClient,
+  useAddresses,
+  Fabric,
+};
+
+export { LocalStream, RemoteStream, Video, useSWClient, useAddresses, Fabric };
