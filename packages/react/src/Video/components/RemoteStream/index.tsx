c6d2b677b91d0724096e7c3e8d2ea38ba06221f2
Vukasin Macura <hyper@Vukasins-MacBook-Pro.local>
2025-08-05 09:51:55 +0200

    Removed commented code from BaseStream and RemoteStream file

--- packages/react/src/Video/components/RemoteStream/index.tsx
+++ packages/react/src/Video/components/RemoteStream/index.tsx
@@ -13,35 +13,3 @@ export type IRemoteStreamProps = {
 export default function RemoteStream(params: IRemoteStreamProps) {
   return <BaseStream {...params} streamSource="remote" />;
 }
-
-
-// import React from 'react';
-// import type * as SignalWire from '@signalwire/client';
-// import BaseStream from '../BaseStream';
-
-// export type IRemoteStreamProps = {
-//   style?: any;
-//   roomSession?: SignalWire.CallSession;
-//   memberStates: Record<string, { isMuted: boolean; isTalking: boolean; hasHandRaised: boolean }>;
-//   userPositions: Record<string, { x: number; y: number; width: number; height: number }>;
-//   address?: any;
-//   updatedCamera?: boolean;
-// };
-
-// /**
-//  * Displays a remote RoomSession stream.
-//  */
-// const RemoteStream = React.memo(function RemoteStream(params: IRemoteStreamProps) {
-//   return <BaseStream key={params?.updatedCamera?.toString()} {...params} streamSource="remote" />;
-// }, (prevProps, nextProps) => {
-//   // Ponovo renderuj samo ako se promeni roomSession, hideVideo ili ako se drastično promene stanja ili pozicije
-//   return (
-//     prevProps.roomSession === nextProps.roomSession &&
-//     prevProps.style === nextProps.style &&
-//     prevProps.address === nextProps.address &&
-//     JSON.stringify(prevProps.memberStates) === JSON.stringify(nextProps.memberStates) &&
-//     JSON.stringify(prevProps.userPositions) === JSON.stringify(nextProps.userPositions)
-//   );
-// });
-
-// export default RemoteStream;
